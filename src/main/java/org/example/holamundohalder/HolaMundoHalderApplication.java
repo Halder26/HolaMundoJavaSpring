f853be32040f2212cf0e186b97956c160f05dfb2
haaldeer26@gmail.com <halderflex26@gmail.com>
2024-02-14 20:26:26 +0000

    Creado

---
+++ src/main/java/org/example/holamundohalder/HolaMundoHalderApplication.java
@@ -0,0 +1,13 @@
+package org.example.holamundohalder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoHalderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaMundoHalderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/holamundohalder/HolaMundoHalderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.holamundohalder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoHalderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
